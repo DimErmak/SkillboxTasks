8616ba7b5b94e8beb89c9777431d9541ea06c110
Ermak <dim.ermak.huaway@gmail.com>
2025-10-30 11:30:49 +0300

    task is ready

--- RegularExpressions/src/org/tasks/one/Main.java
+++ RegularExpressions/src/org/tasks/one/Main.java
@@ -1,7 +1,18 @@
 package org.tasks.one;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
 
+        int size = 31;
+        char[][] twoDimensionalArray = new char[size][size];
+        for (int i = 0; i < size; i++) {
+            System.out.println(" ");
+            for (int j = size - 1; j >= 0; j--) {
+                if ((j == (size - (i + 1))) || (i == j)) twoDimensionalArray[i][j] = 'X';
+                else twoDimensionalArray[i][j] = ' ';
+                System.out.print(twoDimensionalArray[i][j]);
+
+            }
+        }
     }
 }
